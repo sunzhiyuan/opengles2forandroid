33fc8a3067281d070bc33593162b2770bc400f87
sunzy0408 <sunzy0408@thundersoft.com>
2014-11-21 15:40:18 +0800

    temp

--- AirHockey1/src/com/airhockey/android/AirHockeyRenderer.java
+++ AirHockey1/src/com/airhockey/android/AirHockeyRenderer.java
@@ -13,12 +13,53 @@ import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glViewport;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.R.integer;
 import android.opengl.GLSurfaceView.Renderer;
 
 public class AirHockeyRenderer implements Renderer {
+    private static final int BYTES_PER_FLOAT = 4;
+    private final FloatBuffer vertexData;
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    public AirHockeyRenderer(){
+        float[] talbeVertices = {
+                0f,0f,
+                0f,14f,
+                9f,14f,
+                9f,0f
+        };
+        float[] tableVerticesWithTriangles = {
+                // Triangle 1
+                0f, 0f,
+                9f, 14f,
+                0f, 14f,
+                
+                // Triangle 2
+                0f, 0f,
+                9f, 0f,
+                9f, 14f,
+                
+                // Line 1
+                0f, 7f,
+                9f, 7f,
+                
+                // Mallets
+                4.5f, 2f,
+                4.5f, 12f
+        };
+        vertexData = ByteBuffer
+                .allocateDirect(tableVerticesWithTriangles.length*BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(tableVerticesWithTriangles);
+                
+    }
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
         // Set the background clear color to red. The first component is
