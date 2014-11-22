1a21c67628a990be440239bf2209e43b9735d840
sunzy <sunzhiyuan30@163.com>
2014-11-22 13:20:39 +0800

    temp

--- AirHockey1/src/com/airhockey/android/AirHockeyRenderer.java
+++ AirHockey1/src/com/airhockey/android/AirHockeyRenderer.java
@@ -5,14 +5,14 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/kbogla for more book information.
-***/
+ ***/
 package com.airhockey.android;
 
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glViewport;
-
+import static android.opengl.GLES20.*;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -20,65 +20,73 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import com.airhockey.android.util.ShaderHelper;
-import com.airhockey.android.util.TextResourceReader;
-
-import android.R.integer;
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
 
+import com.airhockey.android.util.LoggerConfig;
+import com.airhockey.android.util.ShaderHelper;
+import com.airhockey.android.util.TextResourceReader;
+
 public class AirHockeyRenderer implements Renderer {
+    private static final String A_POSITION = "a_Position";
+    private int aPositionLocation;
+    private static final String U_COLOR = "u_Color";
+    private int uColorLocation;
     private int program;
     private final Context context;
     private static final int BYTES_PER_FLOAT = 4;
     private final FloatBuffer vertexData;
     private static final int POSITION_COMPONENT_COUNT = 2;
-    public AirHockeyRenderer(Context context){
+
+    public AirHockeyRenderer(Context context) {
         this.context = context;
-        float[] talbeVertices = {
-                0f,0f,
-                0f,14f,
-                9f,14f,
-                9f,0f
-        };
+        float[] talbeVertices = { 0f, 0f, 0f, 14f, 9f, 14f, 9f, 0f };
         float[] tableVerticesWithTriangles = {
-                // Triangle 1
-                0f, 0f,
-                9f, 14f,
-                0f, 14f,
-                
-                // Triangle 2
-                0f, 0f,
-                9f, 0f,
-                9f, 14f,
-                
-                // Line 1
-                0f, 7f,
-                9f, 7f,
-                
-                // Mallets
-                4.5f, 2f,
-                4.5f, 12f
-        };
-        vertexData = ByteBuffer
-                .allocateDirect(tableVerticesWithTriangles.length*BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
+            // Triangle 1
+            0f, 0f, 9f, 14f, 0f, 14f,
+
+            // Triangle 2
+            0f, 0f, 9f, 0f, 9f, 14f,
+
+            // Line 1
+            0f, 7f, 9f, 7f,
+
+            // Mallets
+            4.5f, 2f, 4.5f, 12f };
+        vertexData =
+            ByteBuffer
+                .allocateDirect(
+                    tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
         vertexData.put(tableVerticesWithTriangles);
-                
+
     }
+
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
         // Set the background clear color to red. The first component is
         // red, the second is green, the third is blue, and the last
         // component is alpha, which we don't use in this lesson.
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-        String vertexShaderSource = TextResourceReader
-                .readTextFileFromResource(context, R.raw.simple_vertex_shader);
-        String fragmentShaderSource = TextResourceReader
-                .readTextFileFromResource(context, R.raw.simple_fragment_shader);
+        String vertexShaderSource =
+            TextResourceReader.readTextFileFromResource(context,
+                R.raw.simple_vertex_shader);
+        String fragmentShaderSource =
+            TextResourceReader.readTextFileFromResource(context,
+                R.raw.simple_fragment_shader);
         int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
-        int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+        int fragmentShader =
+            ShaderHelper.compileFragmentShader(fragmentShaderSource);
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program);
+        }
+        glUseProgram(program);
+        uColorLocation = glGetUniformLocation(program, U_COLOR);
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
+        
     }
 
     /**
@@ -106,5 +114,13 @@ public class AirHockeyRenderer implements Renderer {
     public void onDrawFrame(GL10 glUnused) {
         // Clear the rendering surface.
         glClear(GL_COLOR_BUFFER_BIT);
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_LINES, 6, 2);
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 8, 1);
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 9, 1);
     }
 }
