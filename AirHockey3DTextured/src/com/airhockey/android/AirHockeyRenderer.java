4969dbd20029098bbaeb85994efef02183c49fdc
sunzy0408 <sunzy0408@thundersoft.com>
2014-11-24 17:51:54 +0800

    err

--- AirHockey3DTextured/src/com/airhockey/android/AirHockeyRenderer.java
+++ AirHockey3DTextured/src/com/airhockey/android/AirHockeyRenderer.java
@@ -21,90 +21,33 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.R.integer;
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
 
+import com.airhockey.android.objects.Mallet;
+import com.airhockey.android.objects.Table;
+import com.airhockey.android.programs.ColorShaderProgram;
+import com.airhockey.android.programs.TextureShaderProgram;
 import com.airhockey.android.util.LoggerConfig;
 import com.airhockey.android.util.MatrixHelper;
 import com.airhockey.android.util.ShaderHelper;
 import com.airhockey.android.util.TextResourceReader;
+import com.airhockey.android.util.TextureHelper;
 
 public class AirHockeyRenderer implements Renderer {
-    private static final String U_MATRIIX = "u_Matrix";
-    private final float [] projectionMatrix = new float[16];
-    private final float [] modelMatrix = new float [16];
-    private int uMatrixLocation;
-    private static final String A_COLOR = "a_Color";
-    private static final int COLOR_COMPONENT_COUNT = 3;
-    private static final String A_POSITION = "a_Position";
-    private int aPositionLocation;
-    private int program;
     private final Context context;
-    private static final int BYTES_PER_FLOAT = 4;
-    private final FloatBuffer vertexData;
-    private static final int POSITION_COMPONENT_COUNT = 2;
-    private static final int STRIDE =
-        (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
-    private int aColorLocation;
+    private final float [] projectionMatrix  = new float[16];
+    private final float [] modelMatrix = new float[16];
+    private Table table;
+    private Mallet mallet;
+    
+    private TextureShaderProgram textureProgram;
+    private ColorShaderProgram colorProgram;
+    private int texture;
     public AirHockeyRenderer(Context context) {
         this.context = context;
-        float[] tableVerticesWithTriangles = {
-            0, 0,1f,1f,1f,
-            -0.5f,-0.5f,0.7f,0.7f,0.7f,
-            0.5f,-0.5f,0.7f,0.7f,0.7f,
-            0.5f,0.5f,0.7f,0.7f,0.7f,
-            -0.5f,0.5f,0.7f,0.7f,0.7f,
-            -0.5f,-0.5f,0.7f,0.7f,0.7f,
-
-            // Line 1
-            -0.5f, 0f, 1f,0f,0f,
-            0.5f, 0f,1f,0f,0f,
-
-            // Mallets
-            0f, -0.25f, 0f,0f,1f,
-            0f, 0.25f ,1f,0f,0f
-            };
-        vertexData =
-            ByteBuffer
-                .allocateDirect(
-                    tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder()).asFloatBuffer();
-        vertexData.put(tableVerticesWithTriangles);
-
     }
-
-    @Override
-    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-        // Set the background clear color to red. The first component is
-        // red, the second is green, the third is blue, and the last
-        // component is alpha, which we don't use in this lesson.
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        String vertexShaderSource =
-            TextResourceReader.readTextFileFromResource(context,
-                R.raw.simple_vertex_shader);
-        String fragmentShaderSource =
-            TextResourceReader.readTextFileFromResource(context,
-                R.raw.simple_fragment_shader);
-        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
-        int fragmentShader =
-            ShaderHelper.compileFragmentShader(fragmentShaderSource);
-        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
-        if (LoggerConfig.ON) {
-            ShaderHelper.validateProgram(program);
-        }
-        glUseProgram(program);
-        aColorLocation = glGetAttribLocation(program, A_COLOR);
-        aPositionLocation = glGetAttribLocation(program, A_POSITION);
-        uMatrixLocation = glGetUniformLocation(program, U_MATRIIX);
-        vertexData.position(0);
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT,
-            GL_FLOAT, false, STRIDE, vertexData);
-        glEnableVertexAttribArray(aPositionLocation);
-        vertexData.position(POSITION_COMPONENT_COUNT);
-        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
-        glEnableVertexAttribArray(aColorLocation);
-    }
-
     /**
      * onSurfaceChanged is called whenever the surface has changed. This is
      * called at least once when the surface is initialized. Keep in mind that
@@ -127,21 +70,30 @@ public class AirHockeyRenderer implements Renderer {
         final float [] temp = new float[16];
         multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix,0);
         System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
+    }
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        // TODO Auto-generated method stub
+        glClearColor(0f, 0f, 0f, 0f);
+        table = new Table();
+        mallet = new Mallet();
+        textureProgram = new TextureShaderProgram(context);
+        colorProgram = new ColorShaderProgram(context);
+        texture = TextureHelper.loadTexture(context, R.drawable.air_hockey_surface);
         
     }
-
-    /**
-     * OnDrawFrame is called whenever a new frame needs to be drawn. Normally,
-     * this is done at the refresh rate of the screen.
-     */
     @Override
-    public void onDrawFrame(GL10 glUnused) {
-        // Clear the rendering surface.
+    public void onDrawFrame(GL10 gl) {
+        // TODO Auto-generated method stub
         glClear(GL_COLOR_BUFFER_BIT);
-        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
-        glDrawArrays(GL_LINES, 6, 2);
-        glDrawArrays(GL_POINTS, 8, 1);
-        glDrawArrays(GL_POINTS, 9, 1);
+        textureProgram.useProgram();
+        textureProgram.setUniforms(projectionMatrix, texture);
+        table.bindData(textureProgram);
+        table.draw();
+        
+        colorProgram.useProgram();
+        colorProgram.setUniforms(projectionMatrix);
+        mallet.bindData(colorProgram);
+        mallet.draw();
     }
 }
