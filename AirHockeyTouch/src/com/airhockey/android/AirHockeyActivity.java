a2d6a12382682f1d810c7d399224fb0232f3b816
sunzy <sunzhiyuan30@163.com>
2014-12-05 20:50:27 +0800

    temp

--- AirHockeyTouch/src/com/airhockey/android/AirHockeyActivity.java
+++ AirHockeyTouch/src/com/airhockey/android/AirHockeyActivity.java
@@ -5,7 +5,7 @@
  * courses, books, articles, and the like. Contact us if you are in doubt.
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/kbogla for more book information.
-***/
+ ***/
 package com.airhockey.android;
 
 import android.app.Activity;
@@ -15,9 +15,13 @@ import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Toast;
 
 public class AirHockeyActivity extends Activity {
+    final AirHockeyRenderer airHockeyRenderer = new AirHockeyRenderer(this);
     /**
      * Hold a reference to our GLSurfaceView
      */
@@ -32,12 +36,13 @@ public class AirHockeyActivity extends Activity {
         // Check if the system supports OpenGL ES 2.0.
         final ActivityManager activityManager =
             (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
- 
+
         final ConfigurationInfo configurationInfo =
             activityManager.getDeviceConfigurationInfo();
         /*
-		
-        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000;
+         * 
+         * final boolean supportsEs2 = configurationInfo.reqGlEsVersion >=
+         * 0x20000;
          */
         // Even though the latest emulator supports OpenGL ES 2.0,
         // it has a bug where it doesn't set the reqGlEsVersion so
@@ -46,19 +51,19 @@ public class AirHockeyActivity extends Activity {
         // OpenGL ES 2.0.
         final boolean supportsEs2 =
             configurationInfo.reqGlEsVersion >= 0x20000
-                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-                 && (Build.FINGERPRINT.startsWith("generic")
-                  || Build.FINGERPRINT.startsWith("unknown")
-                  || Build.MODEL.contains("google_sdk")
-                  || Build.MODEL.contains("Emulator")
-                  || Build.MODEL.contains("Android SDK built for x86")));
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 && (Build.FINGERPRINT
+                    .startsWith("generic")
+                    || Build.FINGERPRINT.startsWith("unknown")
+                    || Build.MODEL.contains("google_sdk")
+                    || Build.MODEL.contains("Emulator") || Build.MODEL
+                        .contains("Android SDK built for x86")));
 
         if (supportsEs2) {
             // Request an OpenGL ES 2.0 compatible context.
             glSurfaceView.setEGLContextClientVersion(2);
 
             // Assign our renderer.
-            glSurfaceView.setRenderer(new AirHockeyRenderer(this));
+            glSurfaceView.setRenderer(airHockeyRenderer);
             rendererSet = true;
         } else {
             /*
@@ -78,6 +83,44 @@ public class AirHockeyActivity extends Activity {
                 Toast.LENGTH_LONG).show();
             return;
         }
+        glSurfaceView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event != null) {           
+                    // Convert touch coordinates into normalized device
+                    // coordinates, keeping in mind that Android's Y
+                    // coordinates are inverted.
+                    final float normalizedX = 
+                        (event.getX() / (float) v.getWidth()) * 2 - 1;
+                    final float normalizedY = 
+                        -((event.getY() / (float) v.getHeight()) * 2 - 1);
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        glSurfaceView.queueEvent(new Runnable() {
+                            
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                airHockeyRenderer.handleTouchPress(
+                                    normalizedX,normalizedY);
+                            }
+                        });
+                    }else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                        glSurfaceView.queueEvent(new Runnable() {
+                            
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                airHockeyRenderer.handleTouchDrag(
+                                    normalizedX,normalizedY);
+                            }
+                        });
+                    }
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
 
         setContentView(glSurfaceView);
     }
@@ -85,7 +128,7 @@ public class AirHockeyActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
-        
+
         if (rendererSet) {
             glSurfaceView.onPause();
         }
@@ -94,7 +137,7 @@ public class AirHockeyActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        
+
         if (rendererSet) {
             glSurfaceView.onResume();
         }
--- AirHockeyTouch/src/com/airhockey/android/AirHockeyRenderer.java
+++ AirHockeyTouch/src/com/airhockey/android/AirHockeyRenderer.java
@@ -19,6 +19,7 @@ import static android.opengl.Matrix.setIdentityM;
 import static android.opengl.Matrix.translateM;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
+import javax.security.auth.PrivateCredentialPermission;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
@@ -28,25 +29,31 @@ import com.airhockey.android.objects.Puck;
 import com.airhockey.android.objects.Table;
 import com.airhockey.android.programs.ColorShaderProgram;
 import com.airhockey.android.programs.TextureShaderProgram;
+import com.airhockey.android.util.Geometry;
+import com.airhockey.android.util.Geometry.Point;
 import com.airhockey.android.util.MatrixHelper;
 import com.airhockey.android.util.TextureHelper;
 
 public class AirHockeyRenderer implements Renderer {
+    private boolean malletPressed = false;
+    private Point blueMalletPosition;
     private final Context context;
-    private final float [] viewMatrix = new float [16];
-    private final float [] viewProjectionMatrix = new float [16];
-    private final float [] modelViewProjectionMatrix = new float [16];
-    private final float [] projectionMatrix  = new float[16];
-    private final float [] modelMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+    private final float[] viewProjectionMatrix = new float[16];
+    private final float[] modelViewProjectionMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] modelMatrix = new float[16];
     private Table table;
     private Mallet mallet;
     private Puck puck;
     private TextureShaderProgram textureProgram;
     private ColorShaderProgram colorProgram;
     private int texture;
+
     public AirHockeyRenderer(Context context) {
         this.context = context;
     }
+
     /**
      * onSurfaceChanged is called whenever the surface has changed. This is
      * called at least once when the surface is initialized. Keep in mind that
@@ -62,31 +69,39 @@ public class AirHockeyRenderer implements Renderer {
     public void onSurfaceChanged(GL10 glUnused, int width, int height) {
         // Set the OpenGL viewport to fill the entire surface.
         glViewport(0, 0, width, height);
-        MatrixHelper.perspectiveM(projectionMatrix, 45, (float)width/(float)height, 1f, 10f);
-        setLookAtM(viewMatrix,0,0f,1.2f,2.2f,0f,0f,0f,0f,1f,0f);
+        MatrixHelper.perspectiveM(projectionMatrix, 45, (float) width
+            / (float) height, 1f, 10f);
+        setLookAtM(viewMatrix, 0, 0f, 1.2f, 2.2f, 0f, 0f, 0f, 0f, 1f, 0f);
     }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         // TODO Auto-generated method stub
         glClearColor(0f, 0f, 0f, 0f);
         table = new Table();
-        mallet = new Mallet(0.08f,0.15f,32);
+        mallet = new Mallet(0.08f, 0.15f, 32);
         puck = new Puck(0.06f, 0.02f, 32);
         textureProgram = new TextureShaderProgram(context);
         colorProgram = new ColorShaderProgram(context);
-        texture = TextureHelper.loadTexture(context, R.drawable.air_hockey_surface);
-        
+        texture =
+            TextureHelper.loadTexture(context, R.drawable.air_hockey_surface);
+        blueMalletPosition = new Point(0f, mallet.height / 2f, 0.4f);
     }
+
     private void positionTableInScene() {
-        setIdentityM(modelMatrix,0);
-        rotateM(modelMatrix,0,-90f,1f,0f,0f);
-        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
+        setIdentityM(modelMatrix, 0);
+        rotateM(modelMatrix, 0, -90f, 1f, 0f, 0f);
+        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0,
+            modelMatrix, 0);
     }
-    private void positionObjectInScene(float x,float y, float z) {
+
+    private void positionObjectInScene(float x, float y, float z) {
         setIdentityM(modelMatrix, 0);
         translateM(modelMatrix, 0, x, y, z);
-        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0, modelMatrix, 0);
+        multiplyMM(modelViewProjectionMatrix, 0, viewProjectionMatrix, 0,
+            modelMatrix, 0);
     }
+
     @Override
     public void onDrawFrame(GL10 gl) {
         // TODO Auto-generated method stub
@@ -97,17 +112,29 @@ public class AirHockeyRenderer implements Renderer {
         textureProgram.setUniforms(modelViewProjectionMatrix, texture);
         table.bindData(textureProgram);
         table.draw();
-        positionObjectInScene(0f,mallet.height/2f,-0.4f);
+        positionObjectInScene(0f, mallet.height / 2f, -0.4f);
         colorProgram.useProgram();
         colorProgram.setUniforms(modelViewProjectionMatrix, 1f, 0f, 0f);
         mallet.bindData(colorProgram);
         mallet.draw();
-        positionObjectInScene(0f,mallet.height/2f,0.4f);
+        positionObjectInScene(0f, mallet.height / 2f, 0.4f);
         colorProgram.setUniforms(modelViewProjectionMatrix, 0f, 0f, 1f);
         mallet.draw();
-        positionObjectInScene(0f,puck.height/2f,0f);
+        positionObjectInScene(0f, puck.height / 2f, 0f);
         colorProgram.setUniforms(modelViewProjectionMatrix, 0.8f, 0.8f, 1f);
         puck.bindData(colorProgram);
         puck.draw();
+    }
+
+    public void handleTouchPress(float normalizedX, float normalizedY) {
+        Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+        Sphere malletBoundingSphere =
+            new Shere(new Point(blueMalletPosition.x, blueMalletPosition.y,
+                blueMalletPosition.z), mallet.height / 2f);
+        malletPressed = Geometry.intersects(malletBoundingSphere,ray);
+    }
+
+    public void handleTouchDrag(float normalizedX, float normalizedY) {
+
     }
 }
