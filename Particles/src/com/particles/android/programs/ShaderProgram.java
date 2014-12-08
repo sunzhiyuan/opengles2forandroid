5433bd5efe13fe8114902e47b71784c3b202c925
sunzy0408 <sunzy0408@thundersoft.com>
2014-12-08 17:09:22 +0800

    temp

--- Particles/src/com/particles/android/programs/ShaderProgram.java
+++ Particles/src/com/particles/android/programs/ShaderProgram.java
@@ -16,7 +16,7 @@ public class ShaderProgram {
     protected static final String A_COLOR = "a_Color";
     protected static final String A_TEXTURE_COORDINATES = "a_TextureCoordinates";
     protected static final String U_TIME = "u_Time";
-    protected static final String A_DIRECTION_VECTOR = "a_DirctionVector";
+    protected static final String A_DIRECTION_VECTOR = "a_DirectionVector";
     protected static final String A_PARTICLE_START_TIME = "a_ParticleStartTime";
     protected final int program;
     protected ShaderProgram(Context context, int vertexShaderResourceId, int fragmentShaderResourceId){
