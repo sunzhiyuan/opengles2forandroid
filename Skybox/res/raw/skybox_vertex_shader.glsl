43ca30c84046b98271c47081f3589a27771d5f8a
sunzy0408 <sunzy0408@thundersoft.com>
2014-12-08 20:28:57 +0800

    temp some spell error`

--- Skybox/res/raw/skybox_vertex_shader.glsl
+++ Skybox/res/raw/skybox_vertex_shader.glsl
@@ -4,7 +4,7 @@ varying vec3 v_Position;
 void main()
 {
     v_Position = a_Position;
-    v_Position = -v_Position.z;
-    gl_Positon = u_Matrix*vec4(a_Position,1.0);
-    gl_Posiotn = gl_Position.xyww;
+    v_Position.z = -v_Position.z;
+    gl_Position = u_Matrix*vec4(a_Position,1.0);
+    gl_Position = gl_Position.xyww;
 }
